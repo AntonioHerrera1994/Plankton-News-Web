1a4027ab632233c22e7d9c9f6437912a11fd7262
AntonioHerrera1994 <herreraquinteroj@gmail.com>
2024-01-30 17:02:42 -0800

    json

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// javascript for index.html
+const blogsDv = document.querySelector('.blogs');
+
+
+
+const url = "http://localhost:3000/blogs";
+
+fetchBlogs();
+
+async function fetchBlogs(){
+    let data = await fetch(url);
+    let blogs = await data.json();
+
+    printBlogs(blogs)
+}
+
+function printBlogs(blogData){
+    blogData.forEach(item =>{
+        let containerB = document.createElement('div');
+        containerB.classList.add('blog');
+
+        containerB.innerHTML = `
+        <h2>${item.tittle}</h2>
+        <p>${item.body}</p>
+        <a href="">Read more...</a>
+        `;
+        
+        blogsDv.appendChild(containerB);
+
+    })
+}
